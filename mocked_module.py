7f8e54748e40024e5be091f718aec093ca9a45eb
Abraham Musa <enyoone3@gmail.com>
2023-07-18 16:44:04 +0100

    added little description in mocked_module.py

--- mocked_module.py
+++ mocked_module.py
@@ -1,4 +1,7 @@
 
+# Please note that both convert_to_upper() in utility.py and mocked_function() below are kept simple in this example to illustrate the concept of mocking. However, in real-world scenarios, these functions could be much more complex, and mocked_function() will have its own complexities that we're not interested in when testing convert_to_upper().
+
+
 def mocked_function():
     # Function implementation goes here
     return "External function result"
